7434e7dcbfeb93e06463d337450360b7a380d460
Hugo Mesquita <hugo.web2@gmail.com>
2017-09-01 18:01:22 -0400

    atualização utlizando svg e correção no menu

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 	<meta charset="UTF-8">
 	<title>Menu Hover</title>
 
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,800" rel="stylesheet">
 	<link rel="stylesheet" href="assets/css/index.css">
 	<link rel="stylesheet" href="assets/css/awesome-4.7.0/css/font-awesome.min.css">
 </head>
@@ -12,7 +13,7 @@
 	
 
 	<span class="icon-menu"><i class="fa fa-bars"></i></span>
-	<span class="icon-close"><i class="fa fa-times"></i></span>
+	<span class="icon-close"><!--<i class="fa fa-times"></i>--></span>
 	
 
 	<nav class="menu">
@@ -25,6 +26,17 @@
 	</nav>
 
 </header>
+
+
+
+<svg id="close" class="icon-close">
+	<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-close">
+		<svg id="icon-close" viewBox="0 0 1000 1000" width="100%" height="100%">
+			<path d="M969.8,870.3c27,27.7,27,71.8,0,99.1C955.7,983,937.9,990,920,990c-17.9,0-35.7-7-49.7-20.7L500,599L129.6,969.4C115.6,983,97.8,990,79.9,990s-35.7-7-49.7-20.7c-27-27.3-27-71.4,0-99.1L400.9,500L30.3,129.3c-27-27.3-27-71.4,0-99.1c27.3-27,71.8-27,99.4,0L500,400.9L870.4,30.2c27.7-27,71.8-27,99.4,0c27,27.7,27,71.8,0,99.1L599.1,500L969.8,870.3z"></path>
+		</svg>	
+
+	</use>
+</svg>
 
 
 	<script src="assets/js/jquery-3.2.1.min.js"></script>
